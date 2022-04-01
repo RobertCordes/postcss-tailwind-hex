23079b7687eb52bae2bd009ffa6d0d242ab68c93
Robert Cordes <robert@diverently.com>
2022-04-01 17:47:19 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# postcss-tailwind-hex
+
+> [PostCSS](https://github.com/postcss/postcss) plugin that replaces Tailwind RGB(a) values with hexadecimal analogues
+
+
+## Processing example
+
+this code
+
+```
+.element {
+    border: 2px solid rgba(0,0,0,.2);
+}
+```
+
+converts to
+
+```
+.element {
+    border: 2px solid #000000;
+}
+```
+
+
+## Installation
+
+```
+npm i --save-dev @diverently/postcss-tailwind-hex
+```
+
+
+## Usage
+
+```
+postcss([ require('postcss-tailwind-hex') ])
+```
+Please refer to [PostCSS documentation](https://github.com/postcss/postcss#usage) for you current environment.
+
+
+### Options
+
+#### `rgbOnly` [Boolean]
+default: `false`
+
+Process only rgb color values
+
+#### `rgbaOnly` [Boolean]
+default: `false`
+
+Process only rgba color values
+
+#### `silent` [Boolean]
+default: `false`
+
+Omit verbose conversion logging
+
+
+## [License](LICENSE)
---
+++ index.js
@@ -0,0 +1,74 @@
+/**
+ * Module RGB(a)-HEX
+ * @type {"postcss"}
+ */
+
+//
+// Module dependencies
+
+var rgb2hex = require('rgb2hex');
+var assign = require('object-assign'); // fixme: remove after postcss node 0.12 drop support
+
+/**
+ * PostCSS plugin
+ * @type {*}
+ */
+module.exports = (opts = {}) => {
+    var reg = /rgba\(\d+%?\s*,\s*\d+%?\s*,\s*\d+%?\s*(,\s*\d?.?\d+|,\s*var\(--tw-(?:.*)-opacity\))?\)/g;
+    var o = assign({}, opts);
+
+    return {
+        postcssPlugin: 'postcss-tailwind-hex',
+        Once(root, { result }) {
+            console.log('Converting Tailwind colors to hex');
+        },
+        Declaration(decl) {
+            var val = decl.value;
+
+            // early return
+            if (!val) {
+                return;
+            }
+
+            // stripping values
+            var rgbValues = val.match(reg);
+
+            // converting values
+            if (rgbValues && rgbValues.length > 0) {
+                var newVal = val;
+
+                rgbValues.forEach(rgb => {
+                    var rgbString = rgb.replace(/,\s*var\(--tw-(.*)-opacity\)/, '');
+                    newVal = newVal.replace(rgb, rgbaToHex(rgbString));
+
+                    if (!o.silent) {
+                        console.info('RGB(a) replaced: ' + rgb + ' -> ' + rgbaToHex(rgbString));
+                    }
+                });
+                decl.value = newVal;
+            }
+        }
+    }
+}
+module.exports.postcss = true
+
+/**
+ * RGBA(a) to hex transformer
+ * @param rgbaString
+ * @returns {string}
+ */
+function rgbaToHex(rgbaString) {
+    var hexString = '';
+
+    hexString = rgb2hex(rgbaString).hex;
+
+    return hexString;
+}
+
+/**
+ * function doing nothing
+ * @returns {boolean}
+ */
+function noop() {
+    return false;
+}
